5f0bf4094d3e9c7446c4380a582b1e1306e3b0be
susapa <>
2013-12-18 13:24:19 +0700

    test

--- sap/test.php
+++ sap/test.php
@@ -1,18 +1,6 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<?php
-$a=array("a"=>"red","b"=>"green","c"=>"blue");
-$b=array_search("red", $a);
-$c=$a[$b];
-$d=in_array("ReD", $a);
-if (in_array("Red", $a)){
-    echo 'มีสี '.$c;
-    echo $d;
-}
-
-?>
-
+ssssssssssssssssssssssss
 </body>
 </html>
